80f0bb1dea16e4c34a18950b074609e788e89af7
jinam Yu <znu@thenet-global.com>
2018-01-02 16:39:59 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ const sass = require('gulp-sass');
 const glob = require('gulp-sass-glob');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+
 /* SASS Option */
 const option = {
-  //*
+  /*
   outputStyle: 'expanded',
   /*/
   outputStyle: 'compressed',
@@ -14,15 +15,72 @@ const option = {
 };
 
 /* TASKS */
-gulp.task('sass', () => gulp.src('./scss/style.scss')
+gulp.task('sass', () => gulp.src('./scss/*.scss')
   .pipe(glob())
   .pipe(sass(option))
-  .pipe(gulp.dest('./dist/css'))
+  .pipe(gulp.dest('./dist'))
+  .pipe(reload({ stream: true }))
+);
+
+// page
+const nunjucks = require('gulp-nunjucks-render');
+const manageEnvironment = (environment) => {
+  environment.addGlobal('merge', () => {
+    'use strict';
+    if (arguments.length < 2) {
+      return {};
+    } else {
+      const obj = {};
+      for (let i = 0, len = arguments.length; i < len; i++) {
+        for (let key in arguments[i]) {
+          if (arguments[i].hasOwnProperty(key)) {
+            obj[key] = arguments[i][key];
+          }
+        }
+      }
+      return obj;
+    }
+  });
+
+  environment.addGlobal('load', (path) => {
+    'use strict';
+    if (fs.lstatSync(path)) {
+      return fs.readFileSync(path, 'utf8').replace(/[\n]?{%[^}]*}[\n]?/g, '');
+    }
+  });
+
+  environment.addGlobal('render', (markup) => {
+    Vue.config.debug = false;
+    Vue.config.productionTip = true;
+    renderer.renderToString(new Vue({
+      template: markup,
+    }), function (err, rendered) {
+      if (err) {
+        markup = error(err);
+        //throw err;
+      } else {
+        markup = beautify.html(correction(rendered), {
+          indent_size: 2,
+          unformatted: ['pre', 'code', 'xmp'],
+          indent_inner_html: true,
+        });
+      }
+    });
+
+    return markup;
+  });
+};
+gulp.task('render', () => gulp.src(['develop/**/*.html'])
+  .pipe(nunjucks({
+    manageEnv: manageEnvironment,
+  }))
+  .pipe(gulp.dest('./'))
   .pipe(reload({ stream: true }))
 );
 
 gulp.task('watch', () => {
     gulp.watch('scss/**/*.scss', { cwd: './' }, ['sass']);
+    gulp.watch('develop/**/*.html', { cwd: './' }, ['render']);
   }
 );
 
@@ -34,5 +92,4 @@ gulp.task('browser-sync', () => {
     },
   });
 });
-
 gulp.task('default', ['sass', 'watch', 'browser-sync']);
