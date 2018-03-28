9d61d6200af78d7e84e988bda889bcf3fbc65cf6
jinam Yu <znu@thenet-global.com>
2018-03-28 15:14:06 +0900

    clean

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const glob = require('gulp-sass-glob');
+
+/* SASS Option */
+const option = {
+  /*
+  outputStyle: 'expanded',
+  /*/
+  outputStyle: 'compressed',
+  //*/
+  includePaths: [''],
+};
+
+/* TASKS */
+gulp.task('scss', () => gulp.src('./scss/*.scss')
+  .pipe(glob())
+  .pipe(sass(option).on('error', sass.logError))
+  .pipe(gulp.dest('./dist'))
+);
